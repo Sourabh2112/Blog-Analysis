750408fb202582b057e06c9551837c37dfb83076
sourabh kumar <sourabhsbg01@gmail.com>
2023-10-04 09:52:43 +0530

    implemented the blog search endpoint

--- controller/search.js
+++ controller/search.js
@@ -1,6 +1,16 @@
-module.exports = (req,res)=>{
-    console.log(req.blogs);
-    res.status(200).json({
-        blog: 0
-    })
-}
+const lodash = require("lodash");
+
+module.exports = (req, res) => {
+  const blogs = req.blogs;
+  function searchBlogs(query) {
+    const lowercaseQuery = query.toLowerCase();
+    return lodash.filter(blogs, (blog) =>
+      lodash.includes(blog.title.toLowerCase(), lowercaseQuery)
+    );
+  }
+  const query = req.query.query;
+  const searchResults = searchBlogs(query);
+  res.status(200).json({
+    searchResults: searchResults,
+  });
+};
--- controller/stat.js
+++ controller/stat.js
@@ -3,33 +3,22 @@ const lodash = require("lodash");
 module.exports = (req, res) => {
   console.log("data retrived succefully");
   const blogs = req.blogs;
+
   // Calculate the total number of blogs fetched
   const blogSize = lodash.size(blogs);
+
   // Find the blog with the longest title
   const longestTitle = lodash.maxBy(blogs, (blog) => blog.title.length);
+
   // Determine the number of blogs with titles containing the word "privacy."
-  const keyword = "privacy"; // The keyword to search for
+  const keyword = "privacy";
   const blogsWithPrivacy = lodash.filter(blogs, (blog) =>
     lodash.includes(blog.title.toLowerCase(), keyword.toLowerCase())
   );
   const numberOfBlogsWithPrivacy = lodash.size(blogsWithPrivacy);
+
   // Create an array of unique blog titles (no duplicates)
   const uniqueBlogTitles = lodash.uniqBy(blogs, "title").map((blog) => blog.title);
-
-  // let len = 0;
-  // let val = ""
-  // for (const key in blogs) {
-  //     console.log(blogs[key])
-  //     break;
-  // }
-  // blogs.foreach(blog => {
-  //     if(blog.title.length>len){
-  //         val = blog.title
-  //         len = blog.title.length
-  //     }
-  // });
-  // console.log('Longest title:', longestTitle ? longestTitle.title : 'No title found');
-  // const longestTitle = lodash.maxBy(strings, 'length');
 
   res.status(200).json({
     longestTitle: longestTitle,
